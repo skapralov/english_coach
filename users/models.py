b11b2dc8683adf1b5ee167d723d27908ed6d7622
skapralov <s.k1990@mail.ru>
2020-12-28 00:45:50 +0300

    add registration and login

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from pydantic import validator
 from fastapi_users import models
 from fastapi_users.authentication import JWTAuthentication
 from fastapi_users.db import TortoiseBaseUserModel, TortoiseUserDatabase
@@ -10,7 +11,26 @@ class User(models.BaseUser):
 
 
 class UserCreate(models.BaseUserCreate):
-    pass
+    @validator('password')
+    def valid_password(cls, value: str):
+        value = value.strip()
+
+        if len(value) < 6:
+            raise ValueError('Length should be at least 6 characters')
+
+        # if len(value) > 20:
+        #     raise ValueError('Length should be not be greater than 20')
+        #
+        # if not any(char.isdigit() for char in value):
+        #     raise ValueError('Password should have at least one numeral')
+        #
+        # if not any(char.isupper() for char in value):
+        #     raise ValueError('Password should have at least one uppercase letter')
+        #
+        # if not any(char.islower() for char in value):
+        #     raise ValueError('Password should have at least one lowercase letter')
+
+        return value
 
 
 class UserUpdate(User, models.BaseUserUpdate):
